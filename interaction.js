87a9bf529e1764ed5fa72578c1c225e9a0cd68bb
abhinavSharma2004 <104728891+abhinavSharma2004@users.noreply.github.com>
2023-04-03 21:11:33 +0530

    Updated site

--- interaction.js
+++ interaction.js
@@ -8,36 +8,11 @@ window.addEventListener('scroll', function() {
     imi.style.opacity = opacity.toString();
   });
 
-  // const plane = document.querySelector('.plane-1-icon');
-  
-  // window.addEventListener('scroll', function() {
-  //     const scrollPosition = window.scrollY;
-  //     plane.style.transform = `translateY(${1 + (scrollPosition / 300)})`;
-  //   });
-
-    // Get the container and image elements
-const contain = document.getElementById('plane-1-icon');
-const image = document.getElementById('plane-1-icon');
-
-// Set the initial position of the image
-let scrollPosition = 0;
-
-// Define the animation function
-function animate() {
-  // Update the scroll position
-  scrollPosition += 5; // Change this value to adjust the scrolling speed
-  
-  // If the image has scrolled completely out of view, reset the position
-  if (scrollPosition > image.width - contain.offsetWidth) {
-    scrollPosition = 0;
-  }
-  
-  // Set the new scroll position
-  contain.scrollLeft = scrollPosition;
-  
-  // Request the next animation frame
-  window.requestAnimationFrame(animate);
+function flight(){
+  const {scrollTop, scrollHeight} = document.documentElement;
+  const dist = scrollTop / (scrollHeight - window.innerHeight) * 92.3 + '%';
+  document.querySelector('.plane-1-icon').style.setProperty('--progress', dist);
+  console.log(scrollPercent);
 }
 
-// Start the animation
-animate();
+document.addEventListener('scroll', flight);
